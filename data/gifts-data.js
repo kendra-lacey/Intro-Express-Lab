8a376c9e24bbabc4ad6845a7f90339aa7bdbc558
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2023-01-03 23:09:34 -0500

    add routes

---
+++ data/gifts-data.js
@@ -0,0 +1,10 @@
+const gifts =[
+  {text: 'Dad', item:'Apple sauce', acquired: false, _id: 7689859},
+  {text: 'Mom', item:'A single cashew', acquired: true, _id: 5869696},
+  {text: 'Sister', item:'Tree branch', acquired: false, _id:1234567},
+  {text: 'Brother', item:'Squirrel', acquired: true, _id:3456789}
+]
+
+export {
+  gifts
+}
--- server.js
+++ server.js
@@ -2,21 +2,34 @@
 
 import express from 'express'
 
+import { gifts } from './data/gifts-data.js'
+
 // Create Express app
 
 const app = express()
 
 // Configure the app (app.set)
 
-
+app.set('view engine', 'ejs')
 
 // Mount Middleware (app.use)
 
 
 
 // Mount routes
+app.get('/', function(req, res) {
+  res.redirect('/home')
+})
 
+app.get('/home', function(req, res) {
+  res.render('home')
+})
 
+app.get('/gifts', function (req, res) {
+  res.render('gifts/index', {
+    gifts: gifts
+  })
+})
 
 // Tell the app to listen on port 3000
 
